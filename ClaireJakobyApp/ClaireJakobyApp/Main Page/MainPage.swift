f5b58a2cc14b57d178b66009004d408a0ef3c114
Claire Jakoby <ce.jakoby@gmail.com>
2018-03-09 13:23:03 +0100

    Slide show op Main page code is af moeten alleen nog foto's in

--- ClaireJakobyApp/ClaireJakobyApp/Main Page/MainPage.swift
+++ ClaireJakobyApp/ClaireJakobyApp/Main Page/MainPage.swift
@@ -9,16 +9,72 @@
 import Foundation
 import UIKit
 
-class MainPage : UIViewController {
+class MainPage : UIViewController, UIScrollViewDelegate {
     
     @IBOutlet var menuButton: UIBarButtonItem!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var scrollView: UIScrollView!
 
     override func viewDidLoad() {
         
+        // Reveal Controller controls
+        //-------------------------
         menuButton.target = self.revealViewController()
         menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
-        
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        
+        
+        // SlideShow Code
+        //------------------------
+        self.scrollView.frame = CGRect(x:0, y:0, width:self.view.frame.width, height:self.view.frame.height)
+        let scrollViewWidth:CGFloat = self.scrollView.frame.width
+        let scrollViewHeight:CGFloat = self.scrollView.frame.height
+        
+        let imgOne = UIImageView(frame: CGRect(x:0, y:0,width:scrollViewWidth, height:scrollViewHeight))
+        imgOne.image = UIImage(named: "XXXXXXX")
+        let imgTwo = UIImageView(frame: CGRect(x:scrollViewWidth, y:0,width:scrollViewWidth, height:scrollViewHeight))
+        imgTwo.image = UIImage(named: "XXXXXXX")
+        let imgThree = UIImageView(frame: CGRect(x:scrollViewWidth*2, y:0,width:scrollViewWidth, height:scrollViewHeight))
+        imgThree.image = UIImage(named: "XXXXXXXX")
+
+        
+        self.scrollView.addSubview(imgOne)
+        self.scrollView.addSubview(imgTwo)
+        self.scrollView.addSubview(imgThree)
+        self.scrollView.contentSize = CGSize(width:self.scrollView.frame.width * 3, height:self.scrollView.frame.height)
+        self.scrollView.delegate = self
+        self.pageControl.currentPage = 0
+        
+        
+        // Timer
+        //------------------------
+        Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(moveToNextPage), userInfo: nil, repeats: true)
     }
     
+  
+    @objc func moveToNextPage (){
+        
+        let pageWidth:CGFloat = self.scrollView.frame.width
+        let maxWidth:CGFloat = pageWidth * 3
+        let contentOffset:CGFloat = self.scrollView.contentOffset.x
+        
+        var slideToX = contentOffset + pageWidth
+        
+        if  contentOffset + pageWidth == maxWidth
+        {
+            slideToX = 0
+        }
+        self.scrollView.scrollRectToVisible(CGRect(x:slideToX, y:0, width:pageWidth, height:self.scrollView.frame.height), animated: true)
+    }
+}
+
+
+private typealias ScrollView = MainPage
+extension ScrollView
+{
+    func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView){
+        let pageWidth:CGFloat = scrollView.frame.width
+        let currentPage:CGFloat = floor((scrollView.contentOffset.x-pageWidth/2)/pageWidth)+1
+        self.pageControl.currentPage = Int(currentPage);
+    }
 }
